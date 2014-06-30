18f5ef6d9c25c264d2482e260d2b8d88da9b377a
Lucas Smolic <lucas.smolic@gmail.com>
2014-06-30 15:28:36 -0700

    adding basic parts to appease the vagrant chef-solo installation

---
+++ roles/base.rb
@@ -0,0 +1,11 @@
+# Name of the role should match the name of the file
+name "base"
+
+# Run list function we mentioned earlier
+run_list(
+#    "recipe[apt]"
+#,
+#    "recipe[apache2]",
+#    "recipe[mysql]",
+#    "recipe[php]"
+)
