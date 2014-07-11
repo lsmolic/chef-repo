6016722995b2dfc0178ee5d684d13fb879da5fee
Lucas Smolic <lucas.smolic@gmail.com>
2014-07-10 18:14:27 -0700

    changing db seed file name and adding more libs to be compiled with php

--- roles/development.rb
+++ roles/development.rb
@@ -3,18 +3,20 @@ name "development"
 
 # Run list function we mentioned earlier
 run_list(
+	"recipe[mysql::server]",
+	"recipe[mysql::client]",
+	"recipe[database]",
+	"recipe[database::mysql]",
 	"recipe[php-xml]",
 	"recipe[php]",
 	"recipe[php-fpm]",
 	"recipe[phpunit]",	
 	"recipe[nginx]",
-	"recipe[python]",
-	"recipe[flask-omaze]",
-	"recipe[thrift-omaze]",
-	"recipe[mysql::server]",
-	"recipe[mysql::client]",
-	"recipe[nodejs]",
-	"recipe[golang]",
+#	"recipe[python]",
+#	"recipe[flask-omaze]",
+#	"recipe[thrift-omaze]",
+#	"recipe[nodejs]",
+#	"recipe[golang]",
 	"recipe[omaze]"
 )
 
@@ -29,11 +31,30 @@ override_attributes({
 		"database" => {
 			"name" => 'omaze_local',
 			"username" => 'root',
-			"password" => ''
+			"password" => '',
+			"seed_file" => "db_minimal.sql"
 		}
 	},
+	"mysql" => {
+		"server_root_password" => ""
+	},
 	"php" => {
-		"configure_options" => ["--with-openssl", "--with-zlib", "--with-curl", "--enable-exif", "--enable-mbstring", "--with-mcrypt", "--enable-ftp", "--with-mysqli", "--with-pdo-mysql", "--with-pear", "--with-config-file-path=/etc", "--enable-fpm", "--enable-opcache", "--with-xsl", "--with-dom"]
+		"install_method" => "source",
+		"url" => "http://museum.php.net/php5",
+		"version" => "5.5.1",
+		"source_packages" => 	["libvpx-devel", "bzip2-devel", "libc-client-devel", "curl-devel", 
+                                "freetype-devel", "gmp-devel", "libjpeg-devel", "krb5-devel", "libmcrypt-devel",
+                                "libpng-devel", "t1lib-devel", "mhash-devel", "php-xml", "php-mysql", "httpd-devel", "pcre-devel",
+                                "net-snmp-devel", "readline-devel", "libXpm-devel", "enchant-devel", "libcurl-devel"],
+		"configure_options" => ["--with-openssl", "--with-zlib", "--with-curl", "--enable-exif", 
+								"--enable-mbstring", "--with-mcrypt", "--enable-ftp", "--with-mysqli", 
+								"--with-pdo-mysql", "--with-pear", "--with-config-file-path=/etc", 
+								"--enable-fpm", "--enable-opcache", "--with-xsl", "--with-vpx-dir=/usr",
+								"--with-gd","--with-jpeg-dir=/usr","--with-png-dir=/usr","--with-xpm-dir=/usr","--with-freetype-dir=/usr",
+								"--with-t1lib","--with-mhash","--with-mysql","--with-xmlrpc","--with-bz2",
+								"--with-gettext","-enable-wddx","--enable-zip","--enable-bcmath","--enable-calendar",
+								"--enable-soap","--enable-sockets","--enable-shmop","--enable-dba",
+								"--enable-sysvmsg","--enable-sysvsem","--enable-sysvshm"]
 	},
 	"php-fpm" => {
 		"user" => "nginx",
@@ -41,7 +62,10 @@ override_attributes({
 		"listen_owner" => "nginx",
 		"listen_group" => "nginx",
 		"pools" => [
-			{ "name" => "wordpress" }
+			{ 
+				"name" => "omaze_local",
+				"catch_workers_output" => "yes"
+			}
 		]
 	},
 	"phpunit" => {},
